4283eed06647f69471331640575c4e75c3acfbf2
Lucas Mendes <lucas.d.mendes@ba.estudante.senai.br>
2025-05-20 11:15:31 -0300

    commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ class Cliente {
 const cliente1 = new Cliente();
 const cliente2 = new Cliente();
 
-cliente1.nome = 'Ricardo';
+cliente1.nome = 'Henrique';
 cliente1.cpf = 11122233309;
 cliente1.agencia = 1001;
 cliente1.saldo = 0;
