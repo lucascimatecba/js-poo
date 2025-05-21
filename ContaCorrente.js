a77c1fdc71448eb4a8bc5f054c050639d8681c7e
Lucas Mendes <lucas.d.mendes@ba.estudante.senai.br>
2025-05-21 11:58:22 -0300

    criação de arquivos para classes

---
+++ ContaCorrente.js
@@ -0,0 +1,16 @@
+export class ContaCorrente {
+    agencia;
+    _saldo = 0;
+    //#saldo = 0
+
+    sacar(valor) {
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+    depositar(valor) {
+        if (valor <= 0) return; // mesmo que    if(valor <= 0) {
+        this._saldo += valor;               //     return;
+    }                                      //   }   
+}
