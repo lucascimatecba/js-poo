e3fcab81261977b7170309ecef6d4326cf1dc621
Lucas Mendes <lucas.d.mendes@ba.estudante.senai.br>
2025-05-21 11:27:16 -0300

    novas funcionalidades

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 class Cliente {
     nome;
     cpf;
+}
+class ContaCorrente {
     agencia;
-    saldo;
+    _saldo = 0;
+    //#saldo = 0
+
+    sacar(valor) {
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+    depositar(valor) {
+        if (valor <= 0) return; // mesmo que    if(valor <= 0) {
+        this._saldo += valor;               //     return;
+    }                                      //   }   
 }
 
 const cliente1 = new Cliente();
 const cliente2 = new Cliente();
 
-cliente1.nome = 'Henrique';
+cliente1.nome = 'Ricardo';
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
 cliente2.nome = 'Alice';
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);  
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(-100); //erro
+
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
+
+console.log(contaCorrenteRicardo);
